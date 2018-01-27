c05c1a2b6f8e1c932a2299252fbe5f961224e309
Rachel <lichterrachel@yahoo.com>
2018-01-27 15:08:08 -0500

    flying scene updated

---
+++ Assets/Scripts/CanvasLoad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasLoad : MonoBehaviour
+
+{
+	public GameObject ScenePick;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		ScenePick.SetActive(true);
+		Time.timeScale = 0;
+
+	}
+}
