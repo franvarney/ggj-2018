ba63baa24e120e23304d8c3ecd1cc3bc2f15ab07
Malcolm Akinje <malcolm.akinje@gmail.com>
2018-01-28 01:20:48 -0500

    Made respawn script. Now have to get respawning to happen.

---
+++ Assets/Scripts/ReSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReSpawn : MonoBehaviour
+{
+
+	[SerializeField] private Image panel;
+
+	public int fadeDuration;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		panel = GetComponent<Image>();
+		Respawn();
+	}
+
+	public void Respawn()
+	{
+		FadeOut();
+		Invoke("FadeIn", fadeDuration + 1);
+	}
+	
+	private void FadeIn()
+	{
+		panel.CrossFadeAlpha(0f, fadeDuration, true);
+	}
+
+
+	private void FadeOut()
+	{
+		panel.CrossFadeAlpha(255f, fadeDuration,true );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
